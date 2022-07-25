89269a3e2eb209f28098f2a7927f241a3449eb6f
abisheks1999 <95606635+abisheks1999@users.noreply.github.com>
2022-07-25 09:28:23 +0530

    Add files via upload

---
+++ springboot-crud-elasticbenstalk/src/main/java/net/springboot/SpringbootCrudRestfulWebservicesApplication.java
@@ -0,0 +1,16 @@
+package net.springboot;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+public class SpringbootCrudRestfulWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudRestfulWebservicesApplication.class, args);
+	}
+
+}
